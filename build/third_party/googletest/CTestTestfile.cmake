29b50fe237e746d76c49ac70b9e33dd8a9554e9e
罗新阳 <luo-xy22@mails.tsinghua.edu.cn>
2025-05-16 17:31:20 +0800

    ignore build/

--- build/third_party/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/course/hpc/users/2022013041/PA3/third_party/googletest
-# Build directory: /home/course/hpc/users/2022013041/PA3/build/third_party/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
