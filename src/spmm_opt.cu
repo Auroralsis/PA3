54113b00da25187d4f23a8c87f8a5eb19afa8a16
罗新阳 <luo-xy22@mails.tsinghua.edu.cn>
2025-05-17 14:24:01 +0800

    debug

--- src/spmm_opt.cu
+++ src/spmm_opt.cu
@@ -3,6 +3,17 @@
 const int WARP_SIZE = 32;
 const int TILE_SIZE = 256;
 
+__global__ void print(int dense_rows, int dense_blocks_num, int *d_dense_order2posi, int *d_dense_bid2order){
+    printf("dense_order2posi\n");
+    for (int i = 0; i < dense_rows; i++) {
+        printf("%d ",d_dense_order2posi[i]);
+    }
+    printf("dense_bid2order\n");
+    for (int i = 0; i < dense_blocks_num; i++) {
+        printf("%d ",d_dense_bid2order[i]);
+    }
+}
+
 __global__ void spmm_kernel_dense_256(int *ptr, int *idx, float *val, float *vin, float *vout,int num_v, int INFEATURE, int *dense_bid2order, int *dense_order2posi, int *sum_of_blocks) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     int bid = blockIdx.x;
@@ -146,14 +157,7 @@ void SpMMOpt::preprocess(float *vin, float *vout) {
 
 void SpMMOpt::run(float *vin, float *vout) {
     // TODO: your code
-    printf("dense_order2posi\n");
-    for (int i = 0; i < dense_rows; i++) {
-        printf("%d ",d_dense_order2posi[i]);
-    }
-    printf("dense_bid2order\n");
-    for (int i = 0; i < dense_blocks_num; i++) {
-        printf("%d ",d_dense_bid2order[i]);
-    }
+    print(dense_rows, dense_blocks_num, d_dense_order2posi, d_dense_bid2order);
     spmm_kernel_dense_256<<<dense_grid, dense_block>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in,
         d_dense_bid2order, d_dense_order2posi, d_sum_of_blocks);
     spmm_kernel_sparse_256<<<sparse_grid, sparse_block>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in,
